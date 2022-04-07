4ff27e53c3700b7d62f92dfbf0e97cf55dffd845
BirdKite <71118045+ChoiSaeyoun@users.noreply.github.com>
2022-04-07 18:23:19 +0900

    2022-04-07-별 찍기-2

---
+++ BOJ/011-2439.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	//freopen("input.txt", "rt", stdin);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - i-1; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+
+	return 0;
+}
