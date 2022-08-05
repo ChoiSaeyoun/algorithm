63c55cb06e8a72de5f65ec07d82f95920e7b82b4
BirdKite <71118045+ChoiSaeyoun@users.noreply.github.com>
2022-08-05 23:19:44 +0900

    2022-08-05-두 수의 합

---
+++ BOJ/032-10807.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	freopen("input.txt", "rt", stdin);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	int arr[105] = { 0 };
+	int v;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	cin >> v;
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i] == v)cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
